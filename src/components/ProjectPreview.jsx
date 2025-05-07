7f0d12cfb376a50a24037e40b0a019c969c91618
Matthew Catalfamo <89117055+MatthewSword7272@users.noreply.github.com>
2025-05-07 16:42:16 +1000

    Edit Projects View

--- src/components/ProjectPreview.jsx
+++ src/components/ProjectPreview.jsx
@@ -1,25 +1,35 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-const ProjectPreview = ({title, description, imageUrl, link, delay}) => {
+const ProjectPreview = ({ title, description, imageUrl, link, delay }) => {
   // const delay = Math.floor(Math.random() * 1000) + 500;
 
   const style = {
     animationDelay: `${delay}ms`,
-  }
+  };
 
   return (
-    <a className={`bg-white shadow-md rounded-lg h-full relative projectView`} style={style} href={link} target="_blank">
-        <img src={imageUrl} alt="Project Thumbnail" className="object-cover rounded-lg" />
-        <div className='absolute text-center inset-0 translate-1/2 bg-gray-50/70 rounded-lg opacity-0 hover:opacity-100 transition-opacity duration-300 flex flex-col items-center justify-center gap-2 p-4'>
-           <div className=''>
-              <h2 className="text-xl font-medium mb-2">{title}</h2>
-              <p className="text-black">{description}</p>
-           </div>
+    <a
+      className={`bg-white shadow-md rounded-lg h-full relative projectView group`}
+      style={style}
+      href={link}
+      target="_blank"
+    >
+      <div className=" relative overflow-hidden rounded-lg">
+        <img
+          src={imageUrl}
+          alt="Project Thumbnail"
+          className="object-cover rounded-lg group-hover:scale-110 duration-300"
+        />
+      </div>
+      <div className="absolute text-center inset-0 translate-1/2 bg-gray-50/80 rounded-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col items-center justify-center gap-2 p-4">
+        <div className="">
+          <h2 className="text-xl font-medium mb-2">{title}</h2>
+          <p className="text-black">{description}</p>
         </div>
+      </div>
     </a>
+  );
+};
 
-  )
-}
-
-export default ProjectPreview
+export default ProjectPreview;
