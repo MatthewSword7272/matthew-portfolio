e5f3bb802501795724fe35e01da5e9eaee113494
Matthew Catalfamo <89117055+MatthewSword7272@users.noreply.github.com>
2025-05-26 10:32:35 +1000

    Changed to Dynamic Imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,19 @@
-import { StrictMode } from "react";
+import { StrictMode, lazy } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import "./index.scss";
-import Home from "./pages/index.jsx";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import AboutMe from "./pages/AboutMe.jsx";
-import Project from "./pages/[slug].jsx";
-import MainLayout from "./layouts/MainLayout.jsx";
-import Projects from "./pages/projects.jsx";
-import AmazingButton from "./pages/amazing_button.jsx";
-import MarqueeComponent from "./pages/marquee.jsx";
-import Scrolling from "./pages/scrolling.jsx";
-import Path from "./pages/path.jsx";
-import SvgAnimate from "./pages/svg_animate.jsx";
-import Transition from "./pages/transition.jsx";
-import Cube from "./pages/cube.jsx";
+const Home = lazy(() => import("./pages/index.jsx"));
+const AboutMe = lazy(() => import("./pages/AboutMe.jsx"));
+// const Project = lazy(() => import("./pages/[slug].jsx"));
+const MainLayout = lazy(() => import("./layouts/MainLayout.jsx"));
+const Projects = lazy(() => import("./pages/projects.jsx"));
+const AmazingButton = lazy(() => import("./pages/amazing_button.jsx"));
+const MarqueeComponent = lazy(() => import("./pages/marquee.jsx"));
+const Scrolling = lazy(() => import("./pages/scrolling.jsx"));
+const Path = lazy(() => import("./pages/path.jsx"));
+const SvgAnimate = lazy(() => import("./pages/svg_animate.jsx"));
+const Transition = lazy(() => import("./pages/transition.jsx"));
+const Cube = lazy(() => import("./pages/cube.jsx"));
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -22,7 +22,7 @@ createRoot(document.getElementById("root")).render(
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/about-me" element={<AboutMe />} />
-        <Route path="/:projectId" element={<Project />} />
+        {/* <Route path="/:projectId" element={<Project />} /> */}
         <Route path="/amazing-button" element={<AmazingButton />} />
         <Route path="/marquee" element={<MarqueeComponent />} />
         <Route path="/scrolling" element={<Scrolling />} />
