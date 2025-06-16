3d2e3b35e843d088b92e2f867bd81d9bb018c3ee
Matthew Catalfamo <89117055+MatthewSword7272@users.noreply.github.com>
2025-06-16 13:27:59 +1000

    Modified the Grid

--- src/pages/dot_grid.jsx
+++ src/pages/dot_grid.jsx
@@ -1,12 +1,12 @@
-import { animate, createTimeline, stagger } from "animejs";
+import { animate, stagger } from "animejs";
 import MainLayout from "../layouts/MainLayout";
+import { useEffect } from "react";
+import "../assets/dot.scss";
 
 const DotGrid = () => {
   return (
     <MainLayout>
-      <div
-        className={"relative grid h-screen place-content-center bg-slate-900"}
-      >
+      <div className={"relative grid h-screen place-content-center bg-slate-900"}>
         <Grid />
       </div>
     </MainLayout>
@@ -20,9 +20,29 @@ const Grid = () => {
   const dots = [];
   let index = 0;
 
+  useEffect(() => {
+    animate(".dot", {
+      scale: [0, 1],
+      opacity: [0, 0.5],
+      delay: stagger(2),
+      easing: "easeOutBack",
+      duration: 700,
+    });
+  }, []);
+
   const handleDotClick = (e) => {
     const index = parseInt(e.currentTarget.dataset.index);
     animate(".dot", {
+      "--h": (_, i) => {
+        // get the relative position in [0, totalDots)
+        const rel = i - index;
+        // turn that into a fraction of the full set
+        const frac = rel / (GRID_WIDTH * GRID_HEIGHT);
+        // multiply by 360° and wrap into [0,360)
+        const hue = (360 * frac) % 360;
+        // ensure positive
+        return hue < 0 ? hue + 360 : hue;
+      },
       scale: [
         { to: 1.35, easing: "easeOutSine", duration: 250 },
         { to: 1, easing: "easeInOutQuad", duration: 500 },
@@ -62,15 +82,11 @@ const Grid = () => {
         <div
           onClick={handleDotClick}
           data-index={index}
-          className={
-            "group cursor-crosshair rounded-full p-2 transition-colors hover:bg-slate-600"
-          }
+          className={"group cursor-crosshair rounded-full p-2 transition-colors hover:bg-slate-600"}
           key={`${i}-${j}`}
         >
           <div
-            className={
-              "dot size-2 rounded-full bg-gradient-to-b from-slate-700 to-slate-400 opacity-50 group-hover:from-indigo-600 group-hover:to-white"
-            }
+            className={"dot size-2 rounded-full opacity-50 group-hover:from-indigo-600 group-hover:to-white"}
             data-index={index}
           />
         </div>
@@ -81,10 +97,7 @@ const Grid = () => {
   }
 
   return (
-    <div
-      style={{ gridTemplateColumns: `repeat(${GRID_WIDTH}, 1fr)` }}
-      className={`grid w-fit`}
-    >
+    <div style={{ gridTemplateColumns: `repeat(${GRID_WIDTH}, 1fr)` }} className={`grid w-fit`}>
       {dots}
     </div>
   );
